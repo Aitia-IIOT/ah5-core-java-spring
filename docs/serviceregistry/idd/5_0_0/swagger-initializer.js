d41dc4bb37585dfdd4cec533816eec88fc1baf88
borditamas <borditamas@gmail.com>
2024-07-18 00:18:55 +0200

    Test swagger export as IDD doc with GitHUB pages

---
+++ docs/serviceregistry/idd/5_0_0/swagger-initializer.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "https://aitia-iiot.github.io/ah5-core-java-spring/serviceregistry/idd/5_0_0/openapi.json",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
