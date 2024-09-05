ccfad520c0f895d7299e536e8be5ed9bafd488aa
Katinka666 <jako.katinka@gmail.com>
2024-09-05 11:51:22 +0200

    create DeviceDiscoveryAPITest.java

---
+++ serviceregistry/src/test/java/eu/arrowhead/serviceregistry/api/http/DeviceDiscoveryAPITest.java
@@ -0,0 +1,14 @@
+package eu.arrowhead.serviceregistry.api.http;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DeviceDiscoveryAPITest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
